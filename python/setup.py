f170420b1b7955b1f664c98cac8b7b74e1110d62
red-eagle-eye <hugoslb@hotmail.com>
2021-04-20 14:55:33 +0100

    pin a specific version for the cryptography package

--- python/setup.py
+++ python/setup.py
@@ -73,7 +73,7 @@ setup(
         'setuptools>=38.5.1',
         'certifi>=2018.1.18',
         'requests>=2.18.4',
-        'cryptography>=2.6.1'
+        'cryptography==2.6.1'
     ],
 
     extras_require={
